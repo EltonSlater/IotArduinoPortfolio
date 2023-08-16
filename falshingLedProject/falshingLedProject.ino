effcd0adc391e1b917472beb41e775c9beb9dff3
EltonSlater <140480102+EltonSlater@users.noreply.github.com>
2023-08-16 15:07:05 +1000

    Update falshingLedProject.ino

--- falshingLedProject/falshingLedProject.ino
+++ falshingLedProject/falshingLedProject.ino
@@ -1,16 +1,17 @@
-int ledPin = 2;   //defines the pin connected to the led
+// defines the pin connected to the led
+int ledPin = 2;
 
 void setup() {
-  // sets pin d2 up redy for output
+  // sets pin d2 up ready for output
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // turns d2 on
   digitalWrite(ledPin, HIGH);
-  // pasues the code for 500 milliseconds
+  // pauses the code for 500 milliseconds
   delay(500);
-  // turns the  d2 pin off
+  // turns the d2 pin off
   digitalWrite(ledPin, LOW);
   // pauses the code for 500 milliseconds
   delay(500);
