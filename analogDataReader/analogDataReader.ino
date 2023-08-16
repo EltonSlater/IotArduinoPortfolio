602523797b636298e37c02a4b455e6c67fc45695
EltonSlater <140480102+EltonSlater@users.noreply.github.com>
2023-08-16 15:39:40 +1000

    Add files via upload

---
+++ analogDataReader/analogDataReader.ino
@@ -0,0 +1,19 @@
+// defines the pin that recives input from the potentiometer
+int inputpin = 2;
+void setup() {
+  // tells the device to be ready to exchange messages at the serial monitor 9600
+  Serial.begin(9600);
+  // gets pin d2 ready to recive input
+  pinMode(inputpin, INPUT);
+  // defines how precise the esp32 is when reading inputs in this case at 12 bits or 4096 individual numbers
+  analogReadResolution(12);
+
+}
+
+void loop() {
+  // print the amount of power being recieved at the pin d2
+  Serial.println(analogRead(inputpin));
+  // pauses the code for 100 milliseconds
+  delay(100);
+
+}
