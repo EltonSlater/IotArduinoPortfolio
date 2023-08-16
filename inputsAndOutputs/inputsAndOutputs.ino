d7c6ba4e81a1c741dc941ffc2860d34ee49d6372
EltonSlater <140480102+EltonSlater@users.noreply.github.com>
2023-08-16 15:21:37 +1000

    Add files via upload

---
+++ inputsAndOutputs/inputsAndOutputs.ino
@@ -0,0 +1,25 @@
+// defines the pin connected to the led
+int ledpin = 4;
+// defines the pin connecte to the switch
+int switchpin = 2;
+
+void setup() {\
+  // sets the pin d2 up to output to the led
+  pinMode(ledpin, OUTPUT);
+  // sets the pin d4 up to get input from the switch
+  pinMode(switchpin, INPUT);
+}
+
+void loop() {
+  // checks if d4 is recieving energy and if it is turns on d2
+  if(digitalRead(switchpin) == HIGH)
+  {
+    digitalWrite(ledpin, HIGH);
+  }
+  // checks if d4 isnt reciving energy and if it isnt turns off d2
+  if(digitalRead(switchpin) != HIGH)
+  {
+    digitalWrite(ledpin, LOW);
+  }
+
+}
